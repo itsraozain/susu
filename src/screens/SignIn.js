667516876c52389f59bb08d921c550d0b11d7be0
Rao Zain <raozain70@gmail.com>
2020-09-27 14:55:30 +0500

    Button

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import {
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
 
 const SignIn = ({ navigation }) => {
   return (
@@ -9,6 +15,28 @@ const SignIn = ({ navigation }) => {
         title="Signup"
         onPress={() => navigation.navigate('signup')}
       />
+      <TouchableOpacity
+        style={{
+          width: Dimensions.get('window').width,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        onPress={() => navigation.navigate('signup')}
+      >
+        <View
+          style={{
+            borderWidth: 2,
+            borderColor: 'black',
+            height: 30,
+            width: 100,
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 15,
+          }}
+        >
+          <Text>SignUp</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
