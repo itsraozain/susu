aa118d3e21b8634bc6ecdaf188edb144c9c7a299
Rao Zain <raozain70@gmail.com>
2020-09-28 21:19:59 +0500

    added component profilescr.js

---
+++ src/components/profilescr.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View, Button } from 'react-native';
+
+const Profilescr = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Profilescr</Text>
+    </View>
+  );
+};
+
+export default Profilescr;
